a1a4a7d1e12feafc4d7f629a23ae242d647f9bad
monisj <koolmonis@gmail.com>
2021-03-29 15:11:45 +0500

    Improved Task1 Lab3

--- src/main/scala/Lab3/Task1_Lab3.scala
+++ src/main/scala/Lab3/Task1_Lab3.scala
@@ -3,33 +3,30 @@ import chisel3._
 import chisel3.util._
 
 class LM_IO_Interface_BranchControl extends Bundle {
-    val fnct3= Input (UInt(3.W))
-    val fnct7 = Input (UInt(7.W))
-    val Aluop=Input(UInt(3.W))
-    val branch= Input ( Bool () )
-    val arg_x= Input ( SInt (32. W ) )
-    val arg_y= Input ( SInt (32. W ) )
+    val fnct3 = Input ( UInt (3. W ) )
+    val branch = Input ( Bool () )
+    val arg_x = Input ( UInt (32. W ) )
+    val arg_y = Input ( UInt (32. W ) )
     val br_taken = Output ( Bool () )
 }
+
 class Task1_Lab3 extends Module {
-    val io = IO ( new LM_IO_Interface_BranchControl )
-    val alu = Module(new Exercise2_Lab3())
-    val alucontrol = Module (new ALU_control())
-
-    //Putting inputs into alucontrol
-    alucontrol.io.Aluop:=io.Aluop
-    alucontrol.io.Func7:=io.fnct7
-    alucontrol.io.Func3:=io.fnct3
-
-    //Now to calculate the branch for ALU
-    alu.io.A:=io.arg_x
-    alu.io.B:=io.arg_y
-    alu.io.Aluop:=alucontrol.io.control
-
-    when (io.branch & alu.io.AluBranch === 1.B){
+    val io = IO (new LM_IO_Interface_BranchControl )
+    io.br_taken:=0.B
+    when(io.branch === 1.B){
+    when((io.arg_x === io.arg_y) && io.fnct3 === "b000".U){
+            io.br_taken:=1.B
+    }.elsewhen((io.arg_x =/= io.arg_y) && io.fnct3 === "b001".U){
+        io.br_taken:=1.B
+    }.elsewhen((io.arg_x < io.arg_y) && (io.fnct3 === "b100".U | io.fnct3 === "b110".U)){
+        io.br_taken:=1.B
+    }.elsewhen((io.arg_x >= io.arg_y) && (io.fnct3 === "b101".U | io.fnct3 === "b111".U)){
         io.br_taken:=1.B
     }.otherwise{
         io.br_taken:=0.B
     }
-
+    }.otherwise{
+        io.br_taken:=0.B
+    }
 }
+
--- src/test/scala/Lab3/Task1_test.scala
+++ src/test/scala/Lab3/Task1_test.scala
@@ -8,12 +8,10 @@ import chisel3.experimental.BundleLiterals._
 class Task1_test extends FreeSpec with ChiselScalatestTester{
     "Test3" in {
         test(new Task1_Lab3){c =>
-            c.io.arg_x.poke(2.S)
-            c.io.arg_y.poke(8.S)
+            c.io.arg_x.poke(2.U)
+            c.io.arg_y.poke(8.U)
             c.io.branch.poke(1.B)
             c.io.fnct3.poke("b110".U)
-            c.io.fnct7.poke(0.U)
-            c.io.Aluop.poke("b010".U)
             c.clock.step(500)
             
             //c.io.result.expect(1.B)
