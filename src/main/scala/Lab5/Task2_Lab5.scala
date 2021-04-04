63547f06b115dc325f8101af6dde5bf67d69ff4a
monisj <koolmonis@gmail.com>
2021-04-04 22:44:24 +0500

    Added ex1 to ex3

--- src/main/scala/Lab5/Task2_Lab5.scala
+++ src/main/scala/Lab5/Task2_Lab5.scala
@@ -1,6 +1,6 @@
 package Lab5
 import chisel3._
-import chisel3.util . _
+import chisel3.util._
 // your code for Transaction_in class
 // your code for Transaction_out class
 class Router [ T <: Data ]( gen : T ) extends Module {
