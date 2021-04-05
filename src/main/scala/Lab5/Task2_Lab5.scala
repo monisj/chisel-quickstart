ce2cb85f388a425cb750557248ffe1007ea5f46c
monisj <koolmonis@gmail.com>
2021-04-05 16:52:39 +0500

    Fixed Task 2

--- src/main/scala/Lab5/Task2_Lab5.scala
+++ src/main/scala/Lab5/Task2_Lab5.scala
@@ -1,11 +1,14 @@
-//package Lab5
-//import chisel3._
-//import chisel3.util._
+package Lab5
+import chisel3._
+import chisel3.util._
 
-//class Transaction_In [T <: Data ](gen1: T) extends Bundle
-
-//class Transaction_Out [T <:Data ](gen2:T) extends Bundle
-
-//class Router [ T <: Data ]( gen : T ) extends Module {
-
-//}
+class Transaction_In [T <: Data ](gen1: T) extends Bundle{
+    val in = Input(gen1)
+    val sel = Input (Bool())
+    val address = Input(UInt(10.W))
+    val out = Output(UInt(32.W))
+}
+class Task2_Lab5 [ T <: Data ]( gen1 : T ) extends Module {
+    val io = IO (new Transaction_In(gen1))
+    io.out := Mux(io.sel,io.in,io.address)
+}
