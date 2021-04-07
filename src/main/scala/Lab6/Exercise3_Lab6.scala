ae98b1d06ff9a8f99bc404ca4c6a942b42939f9f
monisj <koolmonis@gmail.com>
2021-04-07 13:32:22 +0500

    Made changes to Exercise 3

--- src/main/scala/Lab6/Exercise3_Lab6.scala
+++ src/main/scala/Lab6/Exercise3_Lab6.scala
@@ -7,21 +7,27 @@ class Exercise3_Lab6 ( val max : Int ) extends Module {
     val in = Input (UInt(4.W))
     val clock_High = Output (Bool())
     })
-    val counter = RegInit (max. U (4.W ) )
-    val done = counter === 0. U
-    val next = WireInit(0.U)
+    val counter = RegInit (max. U (4.W))
+    val counter_2 = RegInit(max.U(4.W))
     io.clock_High:=0.B
-    when (!done){
-        next:=counter - 1.U
-         io.clock_High:=0.B
-    }.elsewhen(done){
-        io.clock_High := 1.B
+    when (counter === max.U){
+        io.clock_High:=1.B
+        counter:=counter-1.U
+        counter_2:=counter_2-1.U
     }.elsewhen(counter === 0.U){
-      //  when(done){
-        //io.clock_High := 1.B
-        //next := counter + 1.U
-        //}.otherwise{
-         //   next:=counter+1.U
-        //}
+        when(counter_2 === 0.U){
+            io.clock_High:=1.B
+            counter_2 := counter_2 +1.U
+        }.elsewhen(counter_2 === max.U){
+            io.clock_High:=1.B
+            counter:=max.U
+        }.otherwise{
+            io.clock_High:=0.B
+            counter_2:=counter_2+1.U
+        }
+    }.otherwise{
+        io.clock_High:=0.B
+        counter_2:=counter_2-1.U
+        counter:=counter-1.U
+    }
 }
-    counter:=next}
--- src/test/scala/Lab6/Ex_test3.scala
+++ src/test/scala/Lab6/Ex_test3.scala
@@ -7,9 +7,10 @@ import chisel3.experimental.BundleLiterals._
 
 class Ex_test3 extends FreeSpec with ChiselScalatestTester{
     "Test6" in {
-        test(new Exercise3_Lab6(15)){c =>
+        test(new Exercise3_Lab6(8)){c =>
             c.io.in.poke(12.U)
-            c.clock.step(500)
+            c.clock.step(9)
+            c.io.clock_High.expect(0.B)
             
             
     }
