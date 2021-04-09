d9f3f24d236844861247687ec99d522d68242f34
monisj <koolmonis@gmail.com>
2021-04-09 21:51:49 +0500

    Finished Task 1

--- src/main/scala/Lab7/Task1_Lab7.scala
+++ src/main/scala/Lab7/Task1_Lab7.scala
@@ -4,34 +4,81 @@ import chisel3.util._
 import chisel3.experimental.ChiselEnum
 
 class Detect_Seq extends Bundle {
-    val in = Input ( Bool () )
-    val out = Output ( Bool () )
+    val f1 = Input ( Bool () )
+    val f2 = Input ( Bool () )
+    val r1 = Input ( Bool () )
+    val r2 = Input ( Bool () )
+    val out = Output ( UInt(4.W))
     
 }
 
 class Task1_Lab7 extends Module {
     val io = IO (new Detect_Seq)
-    val s0 :: s1 :: s2 :: Nil = Enum (3)
+    val s0 :: s1 :: s2 :: s3 :: Nil = Enum (4)
+    val s4 = 4.U
+    val s5 = 5.U
     val state = RegInit ( s0 )
-
-    io.out:=(state===s0)&(io.in)
-    switch ( state ){
-        is ( s0 ) {
-            when ( io . in ) {
-                state := s1
-            }
-        }
-        is ( s1 ) {
-            when ( !io . in ) {
-                state := s2
-            } .otherwise {
-                state := s0
-            }
-        }
-        is ( s2 ) {
-            when (! io . in ) {
-                state := s0
-            }
-        }
+    io.out:=0.U
+    
+    when ((state === s2 && (io.f1 === false.B && io.r1 === false.B)) || (state === s2 && (io.f1 === true.B)) || (state === s2 && (io.f1 === false.B) && (io.r1 === true.B))){
+        io.out:=3.U
+    }.elsewhen((state === s4 && (io.f2 === true.B)) || (state === s4 && (io.f2 === false.B && io.r2 === false.B)) || (state === s4 && (io.f2 === false.B ) && (io.r2 === true.B))){
+        io.out:=7.U
+    }.otherwise{
+        io.out:=0.U
     }
+
+    switch (state) {
+        is (s0) {
+            when(io.f1===false.B && io.f2 === false.B){
+                state:=s0
+            }.elsewhen((io.f1 === true.B) && (io.f2 === false.B)){
+                state:=s1
+            }.elsewhen((io.f1 === false.B) && (io.f2 === true.B)){
+                state:=s5
+            }.elsewhen((io.f1 === true.B) && (io.f2 === true.B)){
+                state:=s1
+            }
+        } 
+        is (s1){
+            when((io.f1===false.B && io.r1 === false.B)){ 
+                state:=s1
+            }.elsewhen(io.f1 === true.B){
+                state:=s2
+            }.elsewhen(((io.f1 === false.B) && (io.r1 === true.B))){
+                state:=s0
+            }
+        } 
+        is (s2) {
+            when(io.f1===false.B && io.r1 === false.B){
+                state:=s2
+            }.elsewhen(io.f1 === true.B){
+                state:=s3
+            }.elsewhen(io.f1 === false.B && io.r1 === true.B){
+                state:=s1
+            }
+        } 
+        is (s3) {
+            state:=s0
+        } 
+        is (s4) {
+            when(io.f2 === true.B){
+                state:=s3
+            }.elsewhen(io.f2===false.B && io.r2 === false.B){
+                state:=s4
+            }.elsewhen(io.f2 ===  false.B && io.r2 === true.B){
+                state:=s5
+            }
+        } 
+        is (s5) {
+            when(io.f2 === true.B){
+                state:=s4
+            }.elsewhen(io.f2===false.B && io.r2 === false.B){
+                state:=s5
+            }.elsewhen(io.f2 === false.B && io.r2 === true.B){
+                state:=s0
+            }
+        }
+        }
 }
+    
--- src/test/scala/Lab7/Task1_test.scala
+++ src/test/scala/Lab7/Task1_test.scala
@@ -8,9 +8,12 @@ import chisel3.experimental.BundleLiterals._
 class Task1_test extends FreeSpec with ChiselScalatestTester{
     "Test7" in {
         test(new Task1_Lab7){c =>
-            c.io.in.poke(true.B)
-            c.clock.step(20)
-            c.io.out.expect(true.B)
+            c.io.f1.poke(true.B)
+            c.io.f2.poke(true.B)
+            c.io.r1.poke(false.B)
+            c.io.r2.poke(true.B)
+            c.clock.step(10)
+            c.io.out.expect(3.U)
             
     }
     }
