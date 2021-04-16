42c655f44c84a3b7970a8e593b75d4c2411619b9
monisj <koolmonis@gmail.com>
2021-04-16 16:55:52 +0500

    Changes to Ex1 Lab8

--- src/main/scala/Lab8/Exercise1_Lab8.scala
+++ src/main/scala/Lab8/Exercise1_Lab8.scala
@@ -14,7 +14,118 @@ class Mem_IO (width : Int = 8) extends Bundle {
 class Exercise1_Lab8(width : Int = 8) extends Module {
     val io = IO ( new Mem_IO (width))
     val mem = SyncReadMem (1024,Vec (4 ,UInt (width.W)))
-    
-    mem.write ( io.addr , io.dataIn , io.mask )
-    io.dataOut := mem.read ( io.addr , io.enable )
+    val mask_mem = Reg(Vec(4,UInt()))
+
+    val bits = Cat(io.mask(3),io.mask(2),io.mask(1),io.mask(0))
+    when(io.write){
+        switch(bits){
+            is(1.U){
+                mask_mem(0):=io.dataIn(0)
+                mask_mem(1):=0.U
+                mask_mem(2):=0.U
+                mask_mem(3):=0.U
+                mem.write(io.addr,mask_mem)
+            }
+            is(2.U){
+                mask_mem(0):=0.U
+                mask_mem(1):=io.dataIn(1)
+                mask_mem(2):=0.U
+                mask_mem(3):=0.U
+                mem.write(io.addr,mask_mem)
+            }
+            is(3.U){
+                mask_mem(0):=io.dataIn(0)
+                mask_mem(1):=io.dataIn(1)
+                mask_mem(2):=0.U
+                mask_mem(3):=0.U
+                mem.write(io.addr,mask_mem)
+            }
+            is(4.U){
+                mask_mem(0):=0.U
+                mask_mem(1):=0.U
+                mask_mem(2):=io.dataIn(2)
+                mask_mem(3):=0.U
+                mem.write(io.addr,mask_mem)
+            }
+            is(5.U){
+                mask_mem(0):=io.dataIn(0)
+                mask_mem(1):=0.U
+                mask_mem(2):=io.dataIn(2)
+                mask_mem(3):=0.U
+                mem.write(io.addr,mask_mem)
+            }
+            is(6.U){
+                mask_mem(0):=0.U
+                mask_mem(1):=io.dataIn(1)
+                mask_mem(2):=io.dataIn(2)
+                mask_mem(3):=0.U
+                mem.write(io.addr,mask_mem)
+            }
+            is(7.U){
+                mask_mem(0):=io.dataIn(0)
+                mask_mem(1):=io.dataIn(1)
+                mask_mem(2):=io.dataIn(2)
+                mask_mem(3):=0.U
+                mem.write(io.addr,mask_mem)
+            }
+            is(8.U){
+                mask_mem(0):=0.U
+                mask_mem(1):=0.U
+                mask_mem(2):=0.U
+                mask_mem(3):=io.dataIn(3)
+                mem.write(io.addr,mask_mem)
+            }
+            is(9.U){
+                mask_mem(0):=io.dataIn(0)
+                mask_mem(1):=0.U
+                mask_mem(2):=0.U
+                mask_mem(3):=io.dataIn(3)
+                mem.write(io.addr,mask_mem)
+            }
+            is(10.U){
+                mask_mem(0):=0.U
+                mask_mem(1):=io.dataIn(1)
+                mask_mem(2):=0.U
+                mask_mem(3):=io.dataIn(3)
+                mem.write(io.addr,mask_mem)
+            }
+            is(11.U){
+                mask_mem(0):=io.dataIn(0)
+                mask_mem(1):=io.dataIn(1)
+                mask_mem(2):=0.U
+                mask_mem(3):=io.dataIn(3)
+                mem.write(io.addr,mask_mem)
+            }
+            is(12.U){
+                mask_mem(0):=0.U
+                mask_mem(1):=0.U
+                mask_mem(2):=io.dataIn(2)
+                mask_mem(3):=io.dataIn(3)
+                mem.write(io.addr,mask_mem)
+            }
+            is(13.U){
+                mask_mem(0):=io.dataIn(0)
+                mask_mem(1):=0.U
+                mask_mem(2):=io.dataIn(2)
+                mask_mem(3):=io.dataIn(3)
+                mem.write(io.addr,mask_mem)
+            }
+            is(14.U){
+                mask_mem(0):=0.U
+                mask_mem(1):=io.dataIn(1)
+                mask_mem(2):=io.dataIn(2)
+                mask_mem(3):=io.dataIn(3)
+                mem.write(io.addr,mask_mem)
+            }
+            is(15.U){
+                mask_mem(0):=io.dataIn(0)
+                mask_mem(1):=io.dataIn(1)
+                mask_mem(2):=io.dataIn(2)
+                mask_mem(3):=io.dataIn(3)
+                mem.write(io.addr,mask_mem)
+            }
+            
+        }
+    }
+    io.dataOut:=mem.read(io.addr,io.enable)
 }
