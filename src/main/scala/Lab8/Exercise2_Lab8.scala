fd0a3ab19fe062d0635e841946c960dc1e77f6bd
monisj <koolmonis@gmail.com>
2021-04-16 16:38:21 +0500

    Made Changes to Ex 2 Lab 8

--- src/main/scala/Lab8/Exercise2_Lab8.scala
+++ src/main/scala/Lab8/Exercise2_Lab8.scala
@@ -6,37 +6,31 @@ class Mem_Input (width : Int = 8) extends Bundle {
     val enable = Input ( Bool () )
     val write = Input ( Bool () )
     val addr = Input ( UInt (10.W))
-    val mask = Input ( Vec (4 , Bool()))
-    val dataIn = Input ( Vec (4 , UInt (width.W)))
-    val dataOut = Output ( Vec (4 , UInt ( width.W)))
-    val rdAddr = Input ( UInt (10. W ) )
-    val wrAddr = Input ( UInt (10. W ) )
+    val mask = Input ( Vec (2 , Bool()))
+    val dataIn = Input ( Vec (2 , UInt (width.W)))
+    val dataOut = Output ( Vec (2 , UInt ( width.W)))
+    
 }
 
-class Exercise2_Lab8(width : Int = 8) extends Module {
+class Exercise2_Lab8(width : Int = 16) extends Module {
     val io = IO ( new Mem_Input (width))
-    val mem = SyncReadMem (1024,Vec (4 ,UInt (width.W)))
-    val wrDataReg = RegNext ( io . dataIn(0) )
-    val wrDataReg1 = RegNext (io. dataIn(1))
-    val doForwardReg = RegNext ( io . wrAddr === io . rdAddr && io .enable )
-    val doForwardReg1 = RegNext ( io . wrAddr =/= io . rdAddr && io .enable )
+    val mem = SyncReadMem (1024,Vec (2 ,UInt (width.W)))
+    val wrDataReg = RegNext ( io . dataIn)
+    val doForwardReg = RegNext ( io.enable === true.B && io.write === true.B )
     val memread =mem.read(io.addr, io. enable)
     io.dataOut(0):=0.U
     io.dataOut(1):=0.U
-    io.dataOut(2):=0.U
-    io.dataOut(3):=0.U
+
     when(io.write){
     mem.write ( io.addr , io.dataIn , io.mask )
     }
 
     when(doForwardReg === 1.U){
-
-    io.dataOut(0):=wrDataReg
-    }.otherwise{
-        when(doForwardReg1 === 1.U){
-    io.dataOut(0):=wrDataReg1
-        }.otherwise{
-            io.dataOut:=memread
+        for (data <- 0 until 2){ //Passing for two bank memory
+            io.dataOut(data):=Mux(doForwardReg,Mux(io.mask(data) , wrDataReg(data),0.U),0.U)
         }
-}
+    }.otherwise{
+            io.dataOut:=memread
+        
+    }
 }
--- src/test/scala/Lab8/Ex_test2.scala
+++ src/test/scala/Lab8/Ex_test2.scala
@@ -10,8 +10,6 @@ class Ex_test2 extends FreeSpec with ChiselScalatestTester{
         test(new Exercise2_Lab8){c =>
             c.io.enable.poke(true.B)
             c.io.write.poke(true.B)
-            c.io.wrAddr.poke(0.U)
-            c.io.rdAddr.poke(0.U)
             c.io.addr.poke(0.U)
             c.io.mask(0).poke(true.B)
             c.io.dataIn(0).poke(2.U)
